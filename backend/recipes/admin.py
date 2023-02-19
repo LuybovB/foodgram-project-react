36a54c581b5718e43d78bad2e8bff447fa08a1d8
LuybovB <grandprinceess7777@gmail.com>
2023-02-19 18:52:18 +0300

    <commit_message>

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -45,21 +45,39 @@ class IngredientLnline(admin.TabularInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    """Отображает рецепты в панели администратора."""
-    list_display = ('author', 'name', 'cooking_time')
-    search_fields = ('name', 'author', 'tags')
-    list_filter = ('author', 'name', 'tags')
-    inlines = (IngredientLnline,)
-    empty_value_display = VALUE_DISPLAY
+    from django.contrib import admin
+    from django.contrib.admin import display
 
-    def count_favorites(self, obj):
-        return obj.favorites.count()
+    from .models import (Favourite, Ingredient, IngredientInRecipe, Recipe,
+                         ShoppingCart, Tag)
 
+    @admin.register(Recipe)
+    class RecipeAdmin(admin.ModelAdmin):
+        list_display = ('name', 'id', 'author', 'added_in_favorites')
+        readonly_fields = ('added_in_favorites',)
+        list_filter = ('author', 'name', 'tags',)
 
-@admin.register(Favorites)
-class FavoriteAdmin(admin.ModelAdmin):
-    """Отображает подписки на авторов в панели администратора."""
-    list_display = ('user', 'recipe')
-    search_fields = ('user',)
-    list_filter = ('user',)
-    empty_value_display = VALUE_DISPLAY
+        @display(description='Количество в избранных')
+        def added_in_favorites(self, obj):
+            return obj.favorites.count()
+
+    @admin.register(Ingredient)
+    class IngredientAdmin(admin.ModelAdmin):
+        list_display = ('name', 'measurement_unit',)
+        list_filter = ('name',)
+
+    @admin.register(Tag)
+    class TagAdmin(admin.ModelAdmin):
+        list_display = ('name', 'color', 'slug',)
+
+    @admin.register(ShoppingCart)
+    class ShoppingCartAdmin(admin.ModelAdmin):
+        list_display = ('user', 'recipe',)
+
+    @admin.register(Favourite)
+    class FavouriteAdmin(admin.ModelAdmin):
+        list_display = ('user', 'recipe',)
+
+    @admin.register(IngredientInRecipe)
+    class IngredientInRecipe(admin.ModelAdmin):
+        list_display = ('recipe', 'ingredient', 'amount',)
