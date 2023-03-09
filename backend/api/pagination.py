316a414906726faaa8890d2bada6ab9092d04386
LuybovB <grandprinceess7777@gmail.com>
2023-03-09 19:11:42 +0300

    комментарий

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
+from foodgram.settings import P_SIZE
 
 
 class CustomPagination(PageNumberPagination):
     page_size_query_param = "limit"
-    page_size = 6
+    page_size = P_SIZE
