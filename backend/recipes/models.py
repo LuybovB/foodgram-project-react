9d80231f179f9022320a27dd17c444c491be0ef9
LuybovB <grandprinceess7777@gmail.com>
2023-03-16 16:42:56 +0000

    комментарий

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
+from colorfield.fields import ColorField
 from django.db import models
 from django.db.models import UniqueConstraint
-from colorfield.fields import ColorField
 
 User = get_user_model()
 
