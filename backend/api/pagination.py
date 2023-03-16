5960974ff024b742c3c1c32a1569c8b98465fb97
LuybovB <grandprinceess7777@gmail.com>
2023-03-16 16:55:39 +0000

    комментарий

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,5 +1,5 @@
-from rest_framework.pagination import PageNumberPagination
 from foodgram.settings import P_SIZE
+from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,20 +3,12 @@ from django.db.models import F
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import IntegerField, SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 
-from recipes.models import (
-    Favourite,
-    Ingredient,
-    IngredientInRecipe,
-    Recipe,
-    ShoppingCart,
-    Tag
-)
+from recipes.models import Ingredient, IngredientInRecipe, Recipe, ShoppingCart, Favourite, Tag
 from users.models import Follow, User
 
 
