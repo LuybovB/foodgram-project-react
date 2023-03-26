a2ca397b7005f2c1d689f2f72d741df44284accf
LuybovB <grandprinceess7777@gmail.com>
2023-03-26 13:49:52 +0000

    комментарий

--- backend/api/views.py
+++ backend/api/views.py
@@ -18,7 +18,7 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .filters import IngredientSearchFilter, RecipeFilter
 from .pagination import CustomPagination
-from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly,
+from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (CustomUserSerializer, FavouriteSerializer,
                           FollowSerializer, IngredientSerializer, 
                           RecipeReadSerializer, RecipeWriteSerializer,
